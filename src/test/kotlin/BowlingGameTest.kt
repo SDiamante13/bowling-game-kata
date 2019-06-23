41055994476140e49d26a7e71a2353b256800438
Steven Diamante <steven@pivotal.io>
2019-06-23 10:12:00 -0500

    empty message

--- src/test/kotlin/BowlingGameTest.kt
+++ src/test/kotlin/BowlingGameTest.kt
@@ -9,4 +9,13 @@ class BowlingGameTest {
     fun `first frame bowler should have a score of 0`() {
         assertThat(bowlingGame.score).isEqualTo(0)
     }
+
+    @Test
+    fun `after 4 rolls (3, 4, 5, 4) the total score is 16`() {
+        bowlingGame.roll(3)
+        bowlingGame.roll(4)
+        bowlingGame.roll(5)
+        bowlingGame.roll(4)
+        assertThat(bowlingGame.score).isEqualTo(16)
+    }
 }
